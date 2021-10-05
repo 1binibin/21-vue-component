54d1c4b7824eaa5f62c11afc121a2102bc526c51
1binibin <namwonbin@gmail.com>
2021-10-05 14:24:00 +0900

    부모 자식 props 전달 - 이벤트 적용

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,20 @@
 	<div class="app-wrapper">
 		<ImgCp :title="title" :src="src"/>
 		<ul class="thumb-wrap">
-			<ThumbCp v-for="v in foods" :key="v.id" :img="v" />
+			<ThumbCp v-for="v in foods" :key="v.id" :img="v" @@change="onChange" />
 		</ul>
 	</div>
 </template>
 
 <script>
 /* 
-! 1. 부모 컴포넌트가 자식 컴포넌트에게 변수를 전달할 때는 v-bind로 전달한다.
+1. 부모 컴포넌트가 자식 컴포넌트에게 변수를 전달할 때는 v-bind로 전달한다.
 2. 자식 컴포넌트는 부모로 부터 props로 변수를 전달 받는다.
+3. 자식 컴포넌트가 부모 컴포넌트에게 변수를 전달할 때는 event로 전달한다.
+4. 부모 컴포넌트는 자식으로 부터 v-on으로 이벤트를 통한 변수를 전달 받는다.
 */
-// export default 는 한개만 보내는거 module.exports = () => {}
-// export 는 보내는거 module.exports = { ?, ? }
+// export default 는 한개만 보내는거 -> module.exports = () => {}
+// export 는 보내는거 -> module.exports = { ?, ? }
 import axios from 'axios'
 import ImgCp from './components/imgCp.vue'
 import ThumbCp from './components/ThumbCp.vue'
@@ -27,6 +29,12 @@ export default {
 			foods: [],
 			title: '',
 			src: '',
+		}
+	},
+	methods: {
+		onChange(v) {
+			this.src = v.src
+			this.title = v.title
 		}
 	},
 	async created() {
@@ -51,6 +59,7 @@ ul, ol {
 			padding: .5em;
 			display: flex;
 			width: 101%;
+			cursor: pointer;
 		}
 	}
 </style>
