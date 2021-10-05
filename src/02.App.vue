05af5c61dd857221a7338f07d10f1da781b8b9b3
1binibin <namwonbin@gmail.com>
2021-10-05 13:16:09 +0900

    img, thumbnail 불러오기

---
+++ src/02.App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="app-wrapper">
+		<ImgCp />
+	</div>
+</template>
+
+<script>
+// export default 는 한개만 보내는거 module.exports = () => {}
+// export 는 보내는거 module.exports = { ?, ? }
+import axios from 'axios'
+import ImgCp from './components/imgCp.vue'
+
+
+export default {
+	name: 'App',
+	components: { ImgCp },
+	beforeCreate() {
+		console.log('beforeCreate')
+	},
+	beforeMount() {
+		console.log('beforeMount')
+	},
+	async created() {
+		console.log('Create')
+		const { data } = await axios.get('/json/foods.json')
+		console.log(data)
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+	.app-wrapper {
+		max-width: 400px;
+		margin: 2em auto;
+	}
+</style>
--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,56 @@
 <template>
-	<div class="alert alert-primary">
-		<h1 class="text-center">
-			<i class="fa fa-bars"></i>
-			Hello World
-			</h1>
+	<div class="app-wrapper">
+		<ImgCp :title="title" :src="src"/>
+		<ul class="thumb-wrap">
+			<ThumbCp v-for="v in foods" :key="v.id" :img="v" />
+		</ul>
 	</div>
 </template>
 
 <script>
+/* 
+! 1. 부모 컴포넌트가 자식 컴포넌트에게 변수를 전달할 때는 v-bind로 전달한다.
+2. 자식 컴포넌트는 부모로 부터 props로 변수를 전달 받는다.
+*/
 // export default 는 한개만 보내는거 module.exports = () => {}
 // export 는 보내는거 module.exports = { ?, ? }
+import axios from 'axios'
+import ImgCp from './components/imgCp.vue'
+import ThumbCp from './components/ThumbCp.vue'
+
 
 export default {
-
+	name: 'App',
+	components: { ImgCp, ThumbCp },
+	data() {
+		return {
+			foods: [],
+			title: '',
+			src: '',
+		}
+	},
+	async created() {
+		const { data } = await axios.get('/json/foods.json')
+		this.foods = data
+		this.title = this.foods[0].title
+		this.src = this.foods[0].src
+	},
 }
 </script>
 
-<style>
-
+<style lang="scss">
+ul, ol {
+	list-style: none;
+}
+	.app-wrapper {
+		max-width: 400px;
+		margin: 2em auto 1em auto;
+		.thumb-wrap {
+			border: 1px solid #efefef;
+			border-radius: .325em;
+			padding: .5em;
+			display: flex;
+			width: 101%;
+		}
+	}
 </style>
